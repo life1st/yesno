367304b1f9e88bae201f107864c964c1e7109561
Yang <jiaoyangwfzc@gmail.com>
2017-08-17 14:46:36 +0800

    add noStateBar on mobile&&update readme.md.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var answer = new Vue({
         tUrl:'#',
         isClick:false
     }
-})
+});
 var sData = ['.','..','...'],
     i = 0,
     dataBf = new Date().getTime(),
@@ -28,10 +28,10 @@ seen.click(function(){
         if(j>8&&j<40){
             answer.content = 'emmmm...'
         }else if(j>40){
-            answer.content = 'you say what?again.'
+            answer.content = 'you say what?again.';
             reset();
         }
-    },400)
+    },400);
 
     body.css('background','#000');
     answer.content = 'Let me think...';
@@ -58,32 +58,21 @@ seen.click(function(){
             $(window).width()<=768?
                 body.css({"background":"url("+tImage+") top","background-size":"100%"}):
                 body.css({"background":"url("+tImage+") left top","background-size":"25%"})
-        }
+        };
         var dataNow = new Date().getTime();
         console.log(dataBf,dataNow);
         $('#time').html('+'+(dataNow-dataBf)+'ms').css('color','blue');
     }).catch(function (error) {
         console.log(error)
     })
-})
+});
 
 function reset() {
     /*恢复<a>标签点击事件*/
     answer.isClick = false;
     /*清除定时器，恢复<a>标签初始文本*/
     clearInterval(t);
-    answer.seen = 'Get.'
+    answer.seen = 'Get.';
 
     j=0;
 }
-
-/*  
-    使用vue数据绑定代替JQuery控制文档（不过要多加载30k数据，多一个http请求。），
-    使用三元运算符代替if语句，代码逻辑更清晰。（由于answer每10000次返回的数据有一个maybe，所以只能使用if）。
-    获取重复的元素使用变量表示，减少运行时间。
-    点击获取按钮后使用setInterval（）更新按钮文字，避免用户认为页面假死。
-    点击后为<a>设置.disable类名（使用css设置pointer-events: none;），避免发生重复点击，也避免重复请求数据。
-    由于图片大小不确定，在ajax获取到图片地址后马上控制页面上的隐藏元素实现预加载。
-    获取图片加载状态，加载完成后才显示在页面上，通过控制用户等待时间避免白屏问题出现。
-    使用onload（）代替setTimeout（），避免固定延迟出现不必要的时间浪费或者图片未加载完出现白屏。
-*/
